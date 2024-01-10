4fc4693c3df5fef2dbcde75914976210977d4bd8
Sherzod <sherzodabdumutalibov2004@gmail.com>
2024-01-10 17:35:03 +0500

    Последнее, что нужно сделать

---
+++ main.js
@@ -0,0 +1,70 @@
+// Подключение к div-элементу
+var div = document.getElementById("str");
+
+function readInput() {
+  // получение значений из input
+  var input = document.getElementById("input_id").value;
+
+  // разбиение по пробелам в массив
+
+  var arr = input.split(" ");
+  var realNumbers = [];
+
+  for (i = 0; i < 3; i++) {
+    if (isNaN(Number(arr[i]))) realNumbers[i] = "Raqam kirit dedima sangaaaa";
+    else realNumbers[i] = arr[i];
+  }
+
+  for (i in arr) {
+    var val = arr[i];
+
+    var dot = val.indexOf(".");
+    var comma = val.indexOf(",");
+
+    console.log("dot", dot, "comma", comma);
+
+    if (dot == true || comma == true) {
+      dot2 = val.slice(-1);
+      comma2 = val.slice(-1);
+
+      console.log("dot2", dot2, "comma2", comma2);
+
+      if (dot2 === "." || comma2 === ",") {
+        div.innerText = "Не получится";
+        console.log(
+          "Не получится",
+          realNumbers[0],
+          realNumbers[1],
+          realNumbers[2]
+        );
+        return;
+      }
+    }
+  }
+
+  output_mf(realNumbers[0], realNumbers[1], realNumbers[2]);
+}
+
+function output_mf(a, b, c) {
+  // условия
+  aMf = a + b > c;
+  bMf = a + c > b;
+  cMf = c + b > a;
+
+  if (aMf == true && bMf == true && cMf == true) {
+    // вывод ответа
+    div.innerText = "Получится";
+  } else {
+    // вывод ответа
+    div.innerText = "Не получится";
+  }
+
+  console.log(a, b, c);
+}
+
+// функция для очистки поля
+function clear12() {
+  document.getElementById("input_id").value = "";
+  div.innerText = "...";
+  console.clear();
+}
--- script/main.js
+++ script/main.js
@@ -1,34 +1,74 @@
+// Подключение к div-элементу
 var div = document.getElementById("str");
 
-// Получение данных
-// function getting_data() {
+document.body.addEventListener('click', function() {
+    console.log('Кликнули на body!');
+});
 
-// }
 
-function output_mf() {
-    // получение значений из input'а
+function readInput() {
+    // получение значений из input
     var input = document.getElementById("input_id").value;
     
     // разбиение по пробелам в массив
     var arr = input.split(' ');
 
-    // присвоение значений к переменным, преобразование типа string в number
-    var a = parseInt(arr['0']);
-    var b = parseInt(arr['1']);
-    var c = parseInt(arr['2']);
 
-    // вычисление
-    var triangle = (a+b) > c ? 'Треугольник' : 'Фигня';
+    var a = Number(arr['0']);
+    var b = Number(arr['1']);
+    var c = Number(arr['2']);  
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        div.innerText = 'Только цифры! Если хочешь ';
+        console.log(isNaN(a), isNaN(b), isNaN(c))
+        return;
+    }
 
-    // вывод ответа
-    div.innerText = triangle;  
+    for (i in arr){
+        var val = arr[i];
+
+        var dot = val.indexOf('.');
+
+        console.log('dot', dot);
+        
+        if (dot == true) {
+            
+            dot2 = val.slice(-1);
+
+            console.log('dot2', dot2);
+
+            if (dot2 === '.') {
+                div.innerText = 'Не получится';
+                console.log('Не получится', a, b, c);
+                return;
+            }
+        }
+    }
+
+    output_mf(a, b, c)
 }
 
-// function () {
+function output_mf(a, b, c) {
+    
+    // условия 
+    aMf = (a + b) > c;
+    bMf = (a + c) > b;
+    cMf = (c + b) > a;
 
-// }
+    if ( aMf && bMf && cMf) {
+        // вывод ответа
+        div.innerText = 'Получится';
+    } else {
+        // вывод ответа
+        div.innerText = 'Не получится'; 
+    } 
+ 
+    console.log(a, b, c)
+}
+
 
 // функция для очистки поля
-// function clear() {
-//     document.getElementById("input_id").value = "";
-// }
+function clear12() {
+    document.getElementById('input_id').value = '';
+    div.innerText = '...';
+    console.clear()
+}
