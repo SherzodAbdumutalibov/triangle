baf9a5a4f1c87f24a37fb1f2e4444b7140107d6c
Sherzod <sherzodabdumutalibov2004@gmail.com>
2024-01-12 07:43:43 +0500

    Доработал. Выводит типы углов треугольников

--- script/main.js
+++ script/main.js
@@ -1,74 +1,121 @@
 // Подключение к div-элементу
-var div = document.getElementById("str");
+var divForWrite = document.getElementById("str");
 
-document.onclick = function() {
-    console.log('Кликнули на body!');
 
-};
-
+// // ** Нужно добаить исключения на кнопки и на инпут.
+// document.onclick = function() {
+//     console.log('Кликнули на body!');
+//     readInput();
+// };
+// let input_id = document.getElementById("input_id");
+// let btn = document.getElementById("btn");
+// let btn_clear = document.getElementById("btn_clear");
 
 function readInput() {
+
     // получение значений из input
     var input = document.getElementById("input_id").value;
     
     // разбиение по пробелам в массив
     var arr = input.split(' ');
 
-
+    // преобразование из String в Number
     var a = Number(arr['0']);
     var b = Number(arr['1']);
-    var c = Number(arr['2']);  
-    if (isNaN(a) || isNaN(b) || isNaN(c)) {
-        div.innerText = 'Только цифры! Если хочешь ';
-        console.log(isNaN(a), isNaN(b), isNaN(c))
-        return;
-    }
+    var c = Number(arr['2']);
 
-    for (i in arr){
-        var val = arr[i];
+    var chisla = [a, b ,c];
+    var sumDegree = (a + b + c) == 180;
 
-        var dot = val.indexOf('.');
-
-        console.log('dot', dot);
-        
-        if (dot == true) {
-            
-            dot2 = val.slice(-1);
-
-            console.log('dot2', dot2);
-
-            if (dot2 === '.') {
-                div.innerText = 'Не получится';
-                console.log('Не получится', a, b, c);
-                return;
-            }
+    if (sumDegree) {
+        // ------------ Проверка на запятую ------------ 
+        if (isNaN(a) || isNaN(b) || isNaN(c)) {
+            divForWrite.innerText = 'Пользуйся точкой';
+            console.log(isNaN(a), isNaN(b), isNaN(c))
+            return;
         }
+        // ---------------------------------------------
+
+
+        // ------------ Проверка на точку --------------
+        for (i in arr){
+            var val = arr[i];   
+
+            var dot = val.indexOf('.');
+            
+            if (dot == true) {
+                
+                dot = val.slice(-1);
+
+                console.log('dot2', dot);
+
+                if (dot === '.') {
+                    divForWrite.innerText = 'После точки должна стоять цифра';
+                    console.log('После точки должна стоять цифра', a, b, c);
+                    return;
+                }
+            }
+        }  console.log(i)
+        // ---------------------------------------------
+
+
+
+        // --------------- ВЫЗОВ ФУНКЦИИ ---------------
+        output_mf(a, b, c);
+        // ---------------------------------------------
+
+
+        // ---------- ОПРЕДЕЛЕНИЕ ТРЕУГОЛЬНИКА ---------
+        for (i in chisla) {
+            var degree = chisla[i];
+            if (degree == 90) {
+                console.log('Это Прямоугольный треугольник');
+                return
+            } else if (a == 60 && b == 60 && c == 60) {
+                console.log('Это Равнобедренный треугольник')
+                return
+            }
+            else if (degree < 90) {
+                console.log('Это Остроугольнй треугольник')
+                return
+            } 
+            else if (degree > 90) {
+                console.log('Это Тупоугольный треугольник')
+                return
+            } 
+            console.log(chisla[i])
+        }
+        
+        // ---------------------------------------------        
+
+    } else{
+        divForWrite.innerText = 'Сумма углов треугольника должна составлять 180 градусов';
+        console.log('Сумма углов треугольника должна составлять 180 градусов');
     }
 
-    output_mf(a, b, c)
 }
+
+
+// функция для вычисления
 function output_mf(a, b, c) {
-    
-    // условия 
+     
     aMf = (a + b) > c;
     bMf = (a + c) > b;
     cMf = (c + b) > a;
 
     if ( aMf && bMf && cMf) {
-        // вывод ответа
-        div.innerText = 'Получится';
+        divForWrite.innerText = 'Получится';
     } else {
-        // вывод ответа
-        div.innerText = 'Не получится'; 
+        divForWrite.innerText = 'Не получится'; 
     } 
  
     console.log(a, b, c)
 }
 
-
 // функция для очистки поля
 function clear12() {
     document.getElementById('input_id').value = '';
-    div.innerText = '...';
+    divForWrite.innerText = '...';
     console.clear()
 }
+
